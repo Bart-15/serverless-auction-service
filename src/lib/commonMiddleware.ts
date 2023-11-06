2f3dd09cedc75bdd95ac5f75d031516588859214
Bart-15 <brat.tabusao@gmail.com>
2023-11-06 23:03:19 +0800

    [Update, Add]: Add @middy/http-cors package

--- src/lib/commonMiddleware.ts
+++ src/lib/commonMiddleware.ts
@@ -1,10 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import middy from '@middy/core';
+import cors from '@middy/http-cors';
 import httpErrorHandler from '@middy/http-error-handler';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 
 import { ProxyHandler } from '../types/handler.types';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default (handler: middy.PluginObject | ProxyHandler | undefined | any) =>
-  middy(handler).use([httpJsonBodyParser(), httpEventNormalizer(), httpErrorHandler()]);
+  middy(handler).use([httpJsonBodyParser(), httpEventNormalizer(), httpErrorHandler(), cors()]);
